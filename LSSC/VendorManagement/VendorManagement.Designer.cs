b04d770936597c8d36c826a64a2541e9ba5aea59
Daniel Cohen <Daniel Cohen>
2017-09-03 14:51:51 +0300

    Add project files.

---
+++ LSSC/VendorManagement/VendorManagement.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LSSC.VendorManagement
+{
+    partial class VendorManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VendorManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 289);
+            this.Name = "VendorManagement";
+            this.Text = "VendorManagement";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
